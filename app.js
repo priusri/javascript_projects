257c16179551f9f6afff7319e0b310d4b9c8a85a
Romila Mukul <romilawork@gmail.com>
2026-03-16 19:17:55 +0530

    learnind about callbacks and promises

--- app.js
+++ app.js
@@ -22,33 +22,51 @@
 // 
 
 // also array of objects is possible
-const classInfo=[
-    { name: "aman", grade:"A+" , city:"Delhi"},
-    {name: "shradha" , grade:"A" , city:"Pune"},
-    {name: "karan" , grade:"O", city:"Mumbai"}
-]
-const max=prompt("enter the max number");
-// console.log(max);
-const random=Math.floor(Math.random()* max)+1;
-// console.log(random);
-let guess=prompt("guess the number");
-while(true){
-    if(guess=="quit"){
-        console.log("user quit");
-        break;
-    }
+// const classInfo=[
+//     { name: "aman", grade:"A+" , city:"Delhi"},
+//     {name: "shradha" , grade:"A" , city:"Pune"},
+//     {name: "karan" , grade:"O", city:"Mumbai"}
+// ]
+// const max=prompt("enter the max number");
+// // console.log(max);
+// const random=Math.floor(Math.random()* max)+1;
+// // console.log(random);
+// let guess=prompt("guess the number");
+// while(true){
+//     if(guess=="quit"){
+//         console.log("user quit");
+//         break;
+//     }
 
-    else if(guess==random){
-        console.log("you are right!! Congrats!!, the number is ",random);
-        break;
-    }
-    else if(guess< random){
-        guess=prompt("your guess was too small , enter a bigger number ")
-    }
-    else if(guess> random){
-        guess=prompt("your guess was too large , guess a smaller number.")
-    }
-else{
-    prompt(("your guess was wrong. please try again!!"));
+//     else if(guess==random){
+//         console.log("you are right!! Congrats!!, the number is ",random);
+//         break;
+//     }
+//     else if(guess< random){
+//         guess=prompt("your guess was too small , enter a bigger number ")
+//     }
+//     else if(guess> random){
+//         guess=prompt("your guess was too large , guess a smaller number.")
+//     }
+// else{
+//     prompt(("your guess was wrong. please try again!!"));
+// }
+// }
+
+
+h1=document.querySelector("h1");
+
+function changeColor(color, delay , nextColorChange){
+    setTimeout(()=>{
+        h1.style.color=color;
+        if(nextColorChange){
+            nextColorChange();
+        }; 
+    }, delay);
 }
-}
+
+changeColor("red", 1000 , ()=>{
+    changeColor("orange", 1000 ,()=>{
+        changeColor("green", 1000 );
+    });
+});
--- app2.js
+++ app2.js
@@ -1,14 +1,30 @@
-function one(){
-    return 1;
-}
+// function one(){
+//     return 1;
+// }
 
-function two(){
-    return one() + one();
-}
+// function two(){
+//     return one() + one();
+// }
 
-function three(){
-    let ans= two() + one();
-    console.log(ans);
-}
+// function three(){
+//     let ans= two() + one();
+//     console.log(ans);
+// }
 
-three();
+// three();
+
+//javascript  is a single threaded language, it can only execute one task at a time. 
+setTimeout(()=>{
+    console.log("just do it");
+},2000);
+
+setTimeout(()=>{
+    console.log("just do it again");
+},2000);
+console.log("hello world");
+//callback functions are functions that are passed as arguments to other functions and are executed after the completion of the parent function.
+//browser c++ is multithreaded, it has a separate thread for handling the UI, another thread for handling the JavaScript code, and another thread for handling the network requests.
+//they are executed in the order they are called, but the callback functions are executed after the completion of the parent function.
+//synchronous code is executed in the order it is called, while asynchronous code is executed after the completion of the parent function.
+//synchronous code is blocking, while asynchronous code is non-blocking.
+//javascript is synchronous, can act as asynchronous with the help of callback functions, promises, and async/await.
