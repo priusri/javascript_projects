e8f555d6b8903ad8db857e1bcd5da46f837ebd99
Romila Mukul <romilawork@gmail.com>
2026-03-17 23:11:25 +0530

    learning about promises

--- app.js
+++ app.js
@@ -65,8 +65,61 @@ function changeColor(color, delay , nextColorChange){
     }, delay);
 }
 
-changeColor("red", 1000 , ()=>{
-    changeColor("orange", 1000 ,()=>{
-        changeColor("green", 1000 );
-    });
-});
+//callback hell is a situation where we have multiple nested callback functions, which makes the code difficult to read and maintain.
+//  It is also known as "pyramid of doom" because of the shape of the code.
+//  It can be avoided by using promises or async/await.
+// changeColor("red", 1000 , ()=>{
+//     changeColor("orange", 1000 ,()=>{
+//         changeColor("green", 1000 , ()=>{
+//             changeColor("yellow", 1000 , ()=>{
+//                changeColor("blue", 1000);
+//             });
+//         });
+//     });
+// });
+//callback nesting ->callback hell
+
+//promises: represents the eventual completion or failure of an asynchronous operation and its resulting value.
+
+// function saveToDatabase(data, success , failure){
+//     let internetSpeed=Math.floor(Math.random()*10)+1;
+//      if(internetSpeed>4){
+//         success();
+       
+//      }
+//      else{
+//         failure();
+//      }
+// }
+
+saveToDatabase("apna college",()=>{
+    console.log("success: your data was saved. ");
+    saveToDatabase("hello world",()=>{
+        console.log("success2: data2 saved.")
+        saveToDatabase("shraddha",()=>{
+            console.log("success3: data saved.");
+        }, ()=>{
+            console.log("failure3: weak connection");
+        })
+    }, ()=>{
+        console.log("failure2 : weak connection.");
+    })
+},()=>{
+console.log("failure: weak connection. data not saved.");
+}); 
+//promises object represents the eventual completion (or failure) of an asynchronous operation
+//  and its result value. they resolve(success) and reject(failure).
+
+function saveToDatabase(data){
+
+    return new Promise((resolve, reject)){
+        let internetSpeed=Math.floor(Math.random()*10)+1;
+        if(internetSpeed > 4){
+            success("data was saved.");
+        }else{
+            failure("failure, weak connection.");
+        }
+    }
+}
+
+saveToDatabase("apna college");
